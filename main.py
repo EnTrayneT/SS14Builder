79fc2a15b9e6d015e5be4b767e5c25898a526515
HarryON-Dev <githubroflanchik@gmail.com>
2024-04-21 16:01:50 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import sys
 import os
 
-# Добавьте путь к директории DLL PyQt5 в переменную PATH
-pyqt5_dll_path = r'C:\PyQt5\Qt\bin'  # Замените на путь к вашей директории DLL PyQt5
+
+pyqt5_dll_path = r'C:\PyQt5\Qt\bin' 
 if pyqt5_dll_path not in os.environ['PATH'].split(os.pathsep):
     os.environ['PATH'] += os.pathsep + pyqt5_dll_path
 
-# Загрузите модуль PyQt5
 import PyQt5
 
 from PyQt5.QtGui import QIcon
@@ -17,7 +16,6 @@ sys.path.append(r'C:\Users\gofne\OneDrive\Рабочий стол\SS14Build\src\
 
 from gui.mainwindow import TutorialWindow  # Импортируем TutorialWindow из mainwindow
 
-# Здесь можно продолжить ваш код
 
 from PyQt5.QtWidgets import QApplication
 from gui.mainwindow import SS14BuilderApp
